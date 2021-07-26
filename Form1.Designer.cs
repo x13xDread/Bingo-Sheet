7c5acb699193e5b20bc400980b4c51f574d6e21f
Miles Weber <milesweber@comcast.net>
2021-07-26 09:49:29 -0400

    Project Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace BingoSheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B4 = new System.Windows.Forms.Button();
+            this.B5 = new System.Windows.Forms.Button();
+            this.I5 = new System.Windows.Forms.Button();
+            this.I4 = new System.Windows.Forms.Button();
+            this.I3 = new System.Windows.Forms.Button();
+            this.I2 = new System.Windows.Forms.Button();
+            this.I1 = new System.Windows.Forms.Button();
+            this.N4 = new System.Windows.Forms.Button();
+            this.N3 = new System.Windows.Forms.Button();
+            this.N2 = new System.Windows.Forms.Button();
+            this.N1 = new System.Windows.Forms.Button();
+            this.G5 = new System.Windows.Forms.Button();
+            this.G4 = new System.Windows.Forms.Button();
+            this.G3 = new System.Windows.Forms.Button();
+            this.G2 = new System.Windows.Forms.Button();
+            this.G1 = new System.Windows.Forms.Button();
+            this.O5 = new System.Windows.Forms.Button();
+            this.O4 = new System.Windows.Forms.Button();
+            this.O3 = new System.Windows.Forms.Button();
+            this.O2 = new System.Windows.Forms.Button();
+            this.O1 = new System.Windows.Forms.Button();
+            this.FreeSpace = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.checkBingo = new System.Windows.Forms.Timer(this.components);
+            this.ResetBoard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(247, 90);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(50, 50);
+            this.B1.TabIndex = 0;
+            this.B1.Text = "button1";
+            this.B1.UseVisualStyleBackColor = true;
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(247, 146);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(50, 50);
+            this.B2.TabIndex = 1;
+            this.B2.Text = "button1";
+            this.B2.UseVisualStyleBackColor = true;
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(247, 202);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(50, 50);
+            this.B3.TabIndex = 2;
+            this.B3.Text = "button1";
+            this.B3.UseVisualStyleBackColor = true;
+            // 
+            // B4
+            // 
+            this.B4.Location = new System.Drawing.Point(247, 258);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(50, 50);
+            this.B4.TabIndex = 3;
+            this.B4.Text = "button1";
+            this.B4.UseVisualStyleBackColor = true;
+            // 
+            // B5
+            // 
+            this.B5.Location = new System.Drawing.Point(247, 314);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(50, 50);
+            this.B5.TabIndex = 4;
+            this.B5.Text = "button1";
+            this.B5.UseVisualStyleBackColor = true;
+            // 
+            // I5
+            // 
+            this.I5.Location = new System.Drawing.Point(303, 314);
+            this.I5.Name = "I5";
+            this.I5.Size = new System.Drawing.Size(50, 50);
+            this.I5.TabIndex = 9;
+            this.I5.Text = "button1";
+            this.I5.UseVisualStyleBackColor = true;
+            // 
+            // I4
+            // 
+            this.I4.Location = new System.Drawing.Point(303, 258);
+            this.I4.Name = "I4";
+            this.I4.Size = new System.Drawing.Size(50, 50);
+            this.I4.TabIndex = 8;
+            this.I4.Text = "button1";
+            this.I4.UseVisualStyleBackColor = true;
+            // 
+            // I3
+            // 
+            this.I3.Location = new System.Drawing.Point(303, 202);
+            this.I3.Name = "I3";
+            this.I3.Size = new System.Drawing.Size(50, 50);
+            this.I3.TabIndex = 7;
+            this.I3.Text = "button1";
+            this.I3.UseVisualStyleBackColor = true;
+            // 
+            // I2
+            // 
+            this.I2.Location = new System.Drawing.Point(303, 146);
+            this.I2.Name = "I2";
+            this.I2.Size = new System.Drawing.Size(50, 50);
+            this.I2.TabIndex = 6;
+            this.I2.Text = "button1";
+            this.I2.UseVisualStyleBackColor = true;
+            // 
+            // I1
+            // 
+            this.I1.Location = new System.Drawing.Point(303, 90);
+            this.I1.Name = "I1";
+            this.I1.Size = new System.Drawing.Size(50, 50);
+            this.I1.TabIndex = 5;
+            this.I1.Text = "button1";
+            this.I1.UseVisualStyleBackColor = true;
+            // 
+            // N4
+            // 
+            this.N4.Location = new System.Drawing.Point(359, 314);
+            this.N4.Name = "N4";
+            this.N4.Size = new System.Drawing.Size(50, 50);
+            this.N4.TabIndex = 14;
+            this.N4.Text = "button1";
+            this.N4.UseVisualStyleBackColor = true;
+            // 
+            // N3
+            // 
+            this.N3.Location = new System.Drawing.Point(359, 258);
+            this.N3.Name = "N3";
+            this.N3.Size = new System.Drawing.Size(50, 50);
+            this.N3.TabIndex = 13;
+            this.N3.Text = "button1";
+            this.N3.UseVisualStyleBackColor = true;
+            // 
+            // N2
+            // 
+            this.N2.Location = new System.Drawing.Point(359, 146);
+            this.N2.Name = "N2";
+            this.N2.Size = new System.Drawing.Size(50, 50);
+            this.N2.TabIndex = 11;
+            this.N2.Text = "button1";
+            this.N2.UseVisualStyleBackColor = true;
+            // 
+            // N1
+            // 
+            this.N1.Location = new System.Drawing.Point(359, 90);
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(50, 50);
+            this.N1.TabIndex = 10;
+            this.N1.Text = "button1";
+            this.N1.UseVisualStyleBackColor = true;
+            // 
+            // G5
+            // 
+            this.G5.Location = new System.Drawing.Point(415, 314);
+            this.G5.Name = "G5";
+            this.G5.Size = new System.Drawing.Size(50, 50);
+            this.G5.TabIndex = 19;
+            this.G5.Text = "button1";
+            this.G5.UseVisualStyleBackColor = true;
+            // 
+            // G4
+            // 
+            this.G4.Location = new System.Drawing.Point(415, 258);
+            this.G4.Name = "G4";
+            this.G4.Size = new System.Drawing.Size(50, 50);
+            this.G4.TabIndex = 18;
+            this.G4.Text = "button1";
+            this.G4.UseVisualStyleBackColor = true;
+            // 
+            // G3
+            // 
+            this.G3.Location = new System.Drawing.Point(415, 202);
+            this.G3.Name = "G3";
+            this.G3.Size = new System.Drawing.Size(50, 50);
+            this.G3.TabIndex = 17;
+            this.G3.Text = "button1";
+            this.G3.UseVisualStyleBackColor = true;
+            // 
+            // G2
+            // 
+            this.G2.Location = new System.Drawing.Point(415, 146);
+            this.G2.Name = "G2";
+            this.G2.Size = new System.Drawing.Size(50, 50);
+            this.G2.TabIndex = 16;
+            this.G2.Text = "button1";
+            this.G2.UseVisualStyleBackColor = true;
+            // 
+            // G1
+            // 
+            this.G1.Location = new System.Drawing.Point(415, 90);
+            this.G1.Name = "G1";
+            this.G1.Size = new System.Drawing.Size(50, 50);
+            this.G1.TabIndex = 15;
+            this.G1.Text = "button1";
+            this.G1.UseVisualStyleBackColor = true;
+            // 
+            // O5
+            // 
+            this.O5.Location = new System.Drawing.Point(471, 314);
+            this.O5.Name = "O5";
+            this.O5.Size = new System.Drawing.Size(50, 50);
+            this.O5.TabIndex = 24;
+            this.O5.Text = "button1";
+            this.O5.UseVisualStyleBackColor = true;
+            // 
+            // O4
+            // 
+            this.O4.Location = new System.Drawing.Point(471, 258);
+            this.O4.Name = "O4";
+            this.O4.Size = new System.Drawing.Size(50, 50);
+            this.O4.TabIndex = 23;
+            this.O4.Text = "button1";
+            this.O4.UseVisualStyleBackColor = true;
+            // 
+            // O3
+            // 
+            this.O3.Location = new System.Drawing.Point(471, 202);
+            this.O3.Name = "O3";
+            this.O3.Size = new System.Drawing.Size(50, 50);
+            this.O3.TabIndex = 22;
+            this.O3.Text = "button1";
+            this.O3.UseVisualStyleBackColor = true;
+            // 
+            // O2
+            // 
+            this.O2.Location = new System.Drawing.Point(471, 146);
+            this.O2.Name = "O2";
+            this.O2.Size = new System.Drawing.Size(50, 50);
+            this.O2.TabIndex = 21;
+            this.O2.Text = "button1";
+            this.O2.UseVisualStyleBackColor = true;
+            // 
+            // O1
+            // 
+            this.O1.Location = new System.Drawing.Point(471, 90);
+            this.O1.Name = "O1";
+            this.O1.Size = new System.Drawing.Size(50, 50);
+            this.O1.TabIndex = 20;
+            this.O1.Text = "button1";
+            this.O1.UseVisualStyleBackColor = true;
+            // 
+            // FreeSpace
+            // 
+            this.FreeSpace.BackColor = System.Drawing.Color.Lime;
+            this.FreeSpace.Enabled = false;
+            this.FreeSpace.Location = new System.Drawing.Point(359, 202);
+            this.FreeSpace.Name = "FreeSpace";
+            this.FreeSpace.Size = new System.Drawing.Size(50, 50);
+            this.FreeSpace.TabIndex = 25;
+            this.FreeSpace.Text = "Free  Space";
+            this.FreeSpace.UseVisualStyleBackColor = false;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(234, 11);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(321, 76);
+            this.TitleLabel.TabIndex = 26;
+            this.TitleLabel.Text = "B I N G O";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResetBoard
+            // 
+            this.ResetBoard.Location = new System.Drawing.Point(303, 379);
+            this.ResetBoard.Name = "ResetBoard";
+            this.ResetBoard.Size = new System.Drawing.Size(162, 32);
+            this.ResetBoard.TabIndex = 27;
+            this.ResetBoard.Text = "Reset Board";
+            this.ResetBoard.UseVisualStyleBackColor = true;
+            this.ResetBoard.Click += new System.EventHandler(this.ResetBoard_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResetBoard);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.FreeSpace);
+            this.Controls.Add(this.O5);
+            this.Controls.Add(this.O4);
+            this.Controls.Add(this.O3);
+            this.Controls.Add(this.O2);
+            this.Controls.Add(this.O1);
+            this.Controls.Add(this.G5);
+            this.Controls.Add(this.G4);
+            this.Controls.Add(this.G3);
+            this.Controls.Add(this.G2);
+            this.Controls.Add(this.G1);
+            this.Controls.Add(this.N4);
+            this.Controls.Add(this.N3);
+            this.Controls.Add(this.N2);
+            this.Controls.Add(this.N1);
+            this.Controls.Add(this.I5);
+            this.Controls.Add(this.I4);
+            this.Controls.Add(this.I3);
+            this.Controls.Add(this.I2);
+            this.Controls.Add(this.I1);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B4;
+        private System.Windows.Forms.Button B5;
+        private System.Windows.Forms.Button I5;
+        private System.Windows.Forms.Button I4;
+        private System.Windows.Forms.Button I3;
+        private System.Windows.Forms.Button I2;
+        private System.Windows.Forms.Button I1;
+        private System.Windows.Forms.Button N4;
+        private System.Windows.Forms.Button N3;
+        private System.Windows.Forms.Button N2;
+        private System.Windows.Forms.Button N1;
+        private System.Windows.Forms.Button G5;
+        private System.Windows.Forms.Button G4;
+        private System.Windows.Forms.Button G3;
+        private System.Windows.Forms.Button G2;
+        private System.Windows.Forms.Button G1;
+        private System.Windows.Forms.Button O5;
+        private System.Windows.Forms.Button O4;
+        private System.Windows.Forms.Button O3;
+        private System.Windows.Forms.Button O2;
+        private System.Windows.Forms.Button O1;
+        private System.Windows.Forms.Button FreeSpace;
+        protected internal System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Timer checkBingo;
+        private System.Windows.Forms.Button ResetBoard;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,322 @@
+﻿/*
+Miles Weber
+Period 8
+4/17/21
+This is my own work MBW
+This program is a bingo sheet that lets users randomize correct values and have a free space in the middle. When the user gets
+a bingo the sheet detects it automatically. The user is granted the ability to reset the board at any point
+
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BingoSheet
+{
+    public partial class Form1 : Form
+    {
+        #region Vars
+        List<List<Button>> grid = new List<List<Button>>();
+        Random rand = new Random();
+        bool bingo = false;
+        #endregion
+        #region Events
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //add event handlers to all buttons
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Click += Button_Click;
+            }
+            //put all the buttons on the grid into the grid list
+            FillGrid();
+
+            //remove the text from every button except free space
+            ResetGrid();
+
+            //set values
+            SetButtonValues();
+           
+        }
+
+        //when any button is clicked that is a bingo button this runs
+        private void Button_Click(object sender, EventArgs e)
+        {
+            //run if the button is associated as a grid button
+            if (((Button)sender).Name.Length == 2)
+            {
+                if (((Button)sender).BackColor == Color.Lime)
+                    ((Button)sender).BackColor = SystemColors.Control;
+                else
+                    ((Button)sender).BackColor = Color.Lime;
+                //check bingo
+                if (bingo == false)
+                {
+                    if (HorizontalBingo() || VerticalBingo() || DiagonalBingo() || PostageBingo())
+                    {
+                        TitleLabel.Text = "You Have Bingo!";
+
+                        //output with voice
+                        SpeechSynthesizer tts = new SpeechSynthesizer();
+                        
+                        tts.Speak("Bingo!");
+                        
+                        tts.Dispose();
+
+                        bingo = true;
+                    }
+                }
+            }
+            
+        }
+        private void ResetBoard_Click(object sender, EventArgs e)
+        {
+            //reset the entire board
+            //remove the text from every button except free space
+            ResetGrid();
+
+            //set values
+            SetButtonValues();
+
+            //set bingo to false
+            bingo = false;
+
+            //reset bingo text
+            TitleLabel.Text = "B I N G O";
+        }
+        #endregion
+        #region methods
+        //adds all buttons to the grid
+        public void FillGrid()
+        {
+            //create columns
+            List<Button> bColumn = new List<Button>();
+            List<Button> iColumn = new List<Button>();
+            List<Button> nColumn = new List<Button>();
+            List<Button> gColumn = new List<Button>();
+            List<Button> oColumn = new List<Button>();
+
+            //fill collumns
+            bColumn.Add(B1);
+            bColumn.Add(B2);
+            bColumn.Add(B3);
+            bColumn.Add(B4);
+            bColumn.Add(B5);
+
+            iColumn.Add(I1);
+            iColumn.Add(I2);
+            iColumn.Add(I3);
+            iColumn.Add(I4);
+            iColumn.Add(I5);
+
+            nColumn.Add(N1);
+            nColumn.Add(N2);
+            nColumn.Add(FreeSpace);
+            nColumn.Add(N3);
+            nColumn.Add(N4);
+
+            gColumn.Add(G1);
+            gColumn.Add(G2);
+            gColumn.Add(G3);
+            gColumn.Add(G4);
+            gColumn.Add(G5);
+
+            oColumn.Add(O1);
+            oColumn.Add(O2);
+            oColumn.Add(O3);
+            oColumn.Add(O4);
+            oColumn.Add(O5);
+
+            //add columns to 2d list
+
+            grid.Add(bColumn);
+            grid.Add(iColumn);
+            grid.Add(nColumn);
+            grid.Add(gColumn);
+            grid.Add(oColumn);
+        }
+
+        //resets all the values on the board and changes the colors of every button to normal
+        public void ResetGrid()
+        {
+
+            foreach (List<Button> column in grid)
+            {
+                foreach (Button button in column)
+                {
+
+                    if (!(button.Name.Equals("freespace", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        button.Text = "";
+                        button.BackColor = SystemColors.Control;
+                    }
+                }
+            }
+        }
+
+        //labels all buttons except the free space with new values
+        public void SetButtonValues()
+        {
+            ResetGrid();
+            //keeps track of values that were already picked
+            List<int> rolledValues = new List<int>();
+            int item = 0;
+            foreach (List<Button> column in grid)
+            {
+                //clear after each column
+                rolledValues.Clear();
+                foreach (Button button in column)
+                {
+                    switch (button.Name.Substring(0, 1))
+                    {
+                        case "B":
+                            item = RollValue(rolledValues, 1, 16);
+                            rolledValues.Add(item);
+                            button.Text = String.Concat("B", item);
+                            break;
+                        case "I":
+                            item = RollValue(rolledValues, 16, 31);
+                            rolledValues.Add(item);
+                            button.Text = String.Concat("I", item);
+                            break;
+                        case "N":
+                            item = RollValue(rolledValues, 31, 46);
+                            rolledValues.Add(item);
+                            button.Text = String.Concat("N", item);
+                            break;
+                        case "G":
+                            item = RollValue(rolledValues, 46, 61);
+                            rolledValues.Add(item);
+                            button.Text = String.Concat("G", item);
+                            break;
+                        case "O":
+                            item = RollValue(rolledValues, 61, 76);
+                            rolledValues.Add(item);
+                            button.Text = String.Concat("O", item);
+                            break;
+
+                    }
+                }
+            }
+        }
+        
+        //rolls a suitable value for a grid space
+        public int RollValue(List<int> rolledValues, int upperBound, int lowerBound)
+        {
+            int result = 0;
+            bool valid = false;
+            //repeat until valid number is rolled
+            while(!valid)
+            {
+                result = rand.Next(upperBound, lowerBound);
+                valid = true;
+                foreach(int item in rolledValues)
+                {
+                    if (item == result)
+                        valid = false;
+                }
+            }
+            return result;
+        }
+        
+        //checks all horizontals for a w
+        public bool HorizontalBingo()
+        {
+            int count = 0;
+            for(int x = 0; x < 5; x++)
+            {
+                for(int y = 0; y < 5; y++)
+                {                    
+                    //if not the free space
+                    if (x != 2 || y != 2)
+                    {
+                        if (grid[y][x].BackColor == Color.Lime)
+                        {
+                            count++;
+                        }
+                    }
+                    else
+                        count++;
+                }
+                if (count == 5)
+                    return true;
+                count = 0;
+            }
+            return false;
+        }
+        //checks all verticals for a w
+        public bool VerticalBingo()
+        {
+            int count = 0;
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    //if not the free space
+                    if (x != 2 || y != 2)
+                    {
+                        if (grid[x][y].BackColor == Color.Lime)
+                        {
+                            count++;
+                        }
+                    }
+                    else
+                        count++;
+                }
+                if (count == 5)
+                    return true;
+                count = 0;
+            }
+            return false;
+        }
+        //checks both diagonals for a w
+        public bool DiagonalBingo()
+        {
+            if (grid[4][0].BackColor == Color.Lime)
+            {
+                if (grid[3][1].BackColor == Color.Lime)
+                    if (grid[1][3].BackColor == Color.Lime)
+                        if (grid[0][4].BackColor == Color.Lime)
+                            return true;
+            }
+            else if (grid[0][0].BackColor == Color.Lime)
+            {
+                if (grid[1][1].BackColor == Color.Lime)
+                    if (grid[3][3].BackColor == Color.Lime)
+                        if (grid[4][4].BackColor == Color.Lime)
+                            return true;
+            }
+                    return false;
+        }
+        //check every 4 corners for bingo
+        public bool PostageBingo()
+        {
+            if (grid[3][0].BackColor == Color.Lime)
+            {
+                if (grid[4][0].BackColor == Color.Lime)
+                    if (grid[3][1].BackColor == Color.Lime)
+                        if (grid[4][1].BackColor == Color.Lime)
+                            return true;
+            }           
+            return false;
+        }
+
+        #endregion
+       
+       
+    }
+}
